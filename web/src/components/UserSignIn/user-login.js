445df9df753c0eabbfe7da730a46505683c474fa
Mariah Marsolek <mamarsolek@gmail.com>
2021-11-25 15:16:43 -0800

    Made pretty

--- web/src/components/UserSignIn/user-login.js
+++ web/src/components/UserSignIn/user-login.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { Component } from 'react';
-import './user-login.css'
+import './user-login.scss'
 
 export default class UserLogIn extends Component{
     state = {
@@ -32,18 +32,28 @@ export default class UserLogIn extends Component{
 
     render(){
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    User Name:
-                    <input type="text" name="name" onChange={this.handleUserChange}/>
-                </label>
-                <label>
-                    Password:
-                    <input type="password" name="pass" onChange={this.handlePassChange}/>
-                </label>
-                <button type="submit" className='login-button'> Log In
-                </button>
-            </form>
+            <div className="login-flex">
+                <div className= "logo" > AuctionHouse Log In</div>
+                <div className="login-container">
+                    <form onSubmit={this.handleSubmit} className="login-form">
+                        <div className= "form-field">
+                        <label className="username"><span className="hidden">Username</span></label>
+                            <input type="text" name="name" onChange={this.handleUserChange} className= "form-input"
+                            placeholder="Username" required/>
+                        </div>
+
+                        <div className="form form-field">
+                            <label className="lock" htmlFor="login-password"><span className="hidden">Password</span></label>
+                            <input id="login-password" type="password" className="form-input" placeholder="Password" required/>
+                        </div>
+
+                        <div className="form-field">
+                            <input type="submit" value="Log in"/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
         )
     }
 }
